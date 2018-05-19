4c8db32c7b0c494f6e11378bd5bfc228d67bdb46
Mariam Chaudry <mariamschaudry1@gmail.com>
2018-05-19 15:36:29 -0400

    more updating

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,17 +8,38 @@ var router = express.Router();
 
 var burger = require("../models/burger.js");
 
+// Create all routes and set up the logic within those routes where required // 
+// ----------------------------------------------------------------------------
+// Index Redirect // 
 
+router.get("/", function(req, res) {
+    res.redirect("/index");  
+});
 
+// Index Page (render all burgers to the DOM) //
+router.get("/index", function (req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = { burgers: data };
+        // console.log(hbsObject); <-- This works! 
+        res.render("index", hbsObject);
+    });
+});
 
+// Create a new burger //
+router.post("/burger/create", function(req, res) {
+    burger.insertOne(req.body.burger_name, function() {
+        res.redirect("/index");
+    });
+});
 
+// Devour a burger //
+router.post("/burger/eat/:id", function(req, res) {    
+    burger.updateOne(req.params.id, function() {
+        res.redirect("/index"); 
+    }); 
+}); 
 
-
-
-
-
-
-
+// ------------------------------------------------------- //
 
 // Export router for server.js to use //
 module.exports = router;
